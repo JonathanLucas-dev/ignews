a726f319d057bac3bedc1536b970a80e946bf7e3
JonathanAssis <62781731+JonathanAssis@users.noreply.github.com>
2022-05-11 20:34:56 -0300

    Configurando o FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,49 @@
+import { query } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import {fauna} from "../../../services/fauna"
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const {email} = user;
+      
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection("users"),
+              {data: {email}}
+            ),
+            query.Get(
+              query.Match(
+                query.Index("user_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch (error) {
+        return false
+        
+      }
+   
+    },
+  }
 })
